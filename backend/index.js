d71c09689fbc10a3aab220df3231c59faf811749
imsahil37 <sahil.iitg26@gmail.com>
2025-12-23 18:22:37 +0000

    meow~

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://bookstore-346y.vercel.app'],
+    origin: true, //['http://localhost:5173', 'https://bookstore-346y.vercel.app'],
     credentials: true
 }))
 
