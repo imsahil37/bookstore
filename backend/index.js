fe7878f1a44727ac71a16e54fb91a65fe6a90394
imsahil37 <sahil.iitg26@gmail.com>
2025-12-23 17:42:09 +0000

    Deployment

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    origin: ['http://localhost:5173', 'https://bookstore-346y.vercel.app'],
     credentials: true
 }))
 
